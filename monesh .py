409effbfce6ba9bab1edf478f60f0cd6f531597b
KAMMARIMONESH123 <129657655+KAMMARIMONESH123@users.noreply.github.com>
2025-02-25 16:01:50 +0530

    Update monesh .py

--- monesh .py
+++ monesh .py
@@ -1,4 +1,4 @@
-'''a = int(input("Enter a number:"))
+a = int(input("Enter a number:"))
 b = int(input("Enter a number:"))
 
 
@@ -6,10 +6,20 @@ b = int(input("Enter a number:"))
 print("Addition:", a+b)
 print("Subtraction:", a-b)
 print("Multiplication:", a*b)
-print("Division:", a/b)'''
+print("Division:", a/b)
 
 
 
 a = input("Enter your first name: ")
 b = input("Enter your last name: ")
 print('Hello', a + ' ' + b + '! Welcome to the Python program.')
+
+
+
+Num = int(input("Enter an integer: "))
+
+# Checking whether the number is even or odd
+if Num % 2 == 0:
+    print(f"{Num} is an even number.")
+else:
+    print(f"{Num} is an odd number.")
