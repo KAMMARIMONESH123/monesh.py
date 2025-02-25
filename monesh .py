5d3d33eafc5cd32f9435b6acda79043ed156a9bf
KAMMARIMONESH123 <129657655+KAMMARIMONESH123@users.noreply.github.com>
2025-02-25 15:49:45 +0530

    Add files via upload

---
+++ monesh .py
@@ -0,0 +1,15 @@
+'''a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+
+
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)'''
+
+
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+print('Hello', a + ' ' + b + '! Welcome to the Python program.')
